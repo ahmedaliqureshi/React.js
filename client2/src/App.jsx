793fe778213f29a48344bff34f7840b63038eb6c
Ahmed <ahmedaliqureshi59@gmail.com>
2026-01-19 10:49:28 +0000

    todo project

---
+++ client2/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import React, {useState} from 'react';
+
+const App = () =>{
+    cost [variableName,setVariableName]=useState(0);
+    return(
+    <>
+    {/*The background colour and home page */}
+      <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+      <div className="max-w-lg w-full bg-gray-800 p-8 rounded-lg shadow-lg">
+      {/* making the text center and font bold */}
+      <h1 className="text-bold mb-6 h-6 text-center font-bold">My day</h1>
+      <div className=""></div>
+       
+
+      
+    </div>
+    </div>
+    </>
+) 
+}
+
+export default App
