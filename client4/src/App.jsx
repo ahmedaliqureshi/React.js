161413410aefddf97f5b04a93240d915aa045d67
Ahmed <ahmedaliqureshi59@gmail.com>
2026-01-24 19:07:59 +0000

    1st version of my resume with react

---
+++ client4/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>    {/*this the empty tag, which will hold all the tags */}
+   <h1>My resume</h1>  {/*this is the head tag, and child tag as well, no styling is done*/}
+   <h3>my Name:- AAQ</h3>  {/**this arer the header3 tag, with no stying done */}
+   <h3>my phonenum:- 89XXXX </h3>
+   <h3>my nationality:- Indian</h3>
+    </>
+  )
+}
+export default App
